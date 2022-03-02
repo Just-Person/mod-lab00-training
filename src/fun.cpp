ec92e52af9569da124c4e34b04117f000b0288b9
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-03-03 01:56:04 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,9 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 int64_t power(int64_t x, uint16_t n) {
- int64_t result = x;
- for (uint16_t i = 0; i < n-1; i++) {
- result = result * x;
- }
- return result;
+int64_t result = x;
+for (uint16_t i = 0; i < n-1; i++) {
+result = result * x;
+}
+return result;
 }
