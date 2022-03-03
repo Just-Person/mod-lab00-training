23cd83e85a5dd1bbc177dc9d366cbaf1e3bfcb98
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-03-03 23:13:34 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 UNN-IASR
-#include "fun.h"
 #include <iostream>
+#include "fun.h"
 
-using namespace std;
 int main() {
 int64_t x = 0;
 uint64_t n = 1;
-cin>>x>>n;
+std::cin >> x >> n;
 return power(x, n);
 }
